b8f32ef57f622441879d108fae0edb8148c00812
Dannielle Taylor <dannielletaylor@outlook.com>
2020-05-09 16:56:51 -0500

    correcting undefined error in Navbar file

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,5 @@
-import React from "react";
-import "../assets/navbar.css";
+import React from "react"
+import "../assets/navbar.css"
 import { Link } from "react-router-dom";
 
 class Navbar extends React.Component {
@@ -7,12 +7,13 @@ class Navbar extends React.Component {
         return (
 
             <nav className="navbar fixed-top">
-                <span className="navbar-brand mb-0 h1">Great Reads Books Search</span>
-                <div className="links">
+                <span className="navbar-brand mb-0 h1">Google Books Search</span>
+                <div>
                     <Link to="/">Search</Link>
                     <Link to="/saved">Saved</Link>
                 </div>
             </nav>
+
         )
     }
 };
