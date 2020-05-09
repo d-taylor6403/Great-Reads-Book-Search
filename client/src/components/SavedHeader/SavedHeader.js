f9be9481ba4068ad4316a8f43260392a24a3da38
Dannielle Taylor <dannielletaylor@outlook.com>
2020-05-08 19:34:21 -0500

    adding compelted savedHeader.js file

---
+++ client/src/components/SavedHeader/SavedHeader.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "SavedHeader.css";
+
+class SavedHeader extends React.Component {
+    render() {
+        return (
+            <div className="savedHeader">
+                <h1>Saved Books</h1>
+                <h2>View Your Saved Books Below</h2>
+            </div>
+        )
+    }
+}
+
+export default SavedHeader
