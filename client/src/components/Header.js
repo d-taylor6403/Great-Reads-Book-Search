f2f4dab384801c621cd2a7086a8c31c1794b3382
Dannielle Taylor <dannielletaylor@outlook.com>
2020-05-09 16:55:58 -0500

    correcting naming conventions to correct undefined error

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,18 +6,14 @@ class Header extends React.Component {
         event.preventDefault()
         this.props.handleSearch(this.props.search)
     }
-
     render() {
         return (
             <div className="header">
-                <h1>Great Reads <br></br>
-                "The Journey of a Lifetime starts with the <br></br>turning of a page." - Rachel Anders</h1>
-                <br></br>
-
-                <h2>Enter the name of a book of interest below</h2>
+                <h1>Google Books Search</h1>
+                <h2>Search for and Save Books of Interest</h2>
                 <form>
                     <div className="input-group">
-                        <input className="form-control" type="text" placeholder="Find A Book" value={this.props.search} onChange={this.props.updateInput}></input>
+                        <input className="form-control" type="text" placeholder="Find a Book" value={this.props.search} onChange={this.props.updateInput}></input>
                         <button className="btn input-group-append" onClick={this.handleSubmit}>Search</button>
                     </div>
                 </form>
