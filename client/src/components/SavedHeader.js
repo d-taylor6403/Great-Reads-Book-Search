0c72818305c21cbca814b2a990998863011862fa
Dannielle Taylor <dannielletaylor@outlook.com>
2020-05-09 16:58:24 -0500

    correcting undefined issue

--- client/src/components/SavedHeader.js
+++ client/src/components/SavedHeader.js
@@ -1,12 +1,12 @@
-import React from "react";
-import "../assets/savedHeader.css";
+import React from "react"
+import "../assets/saved.css"
 
 class SavedHeader extends React.Component {
     render() {
         return (
             <div className="savedHeader">
                 <h1>Saved Books</h1>
-                <h2>View Your Saved Books Below</h2>
+                <h2>View your Saved Books Below</h2>
             </div>
         )
     }
