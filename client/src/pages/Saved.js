bdb5333ec38cf9111f06f78ca1930d594c07c8e5
Dannielle Taylor <dannielletaylor@outlook.com>
2020-05-08 21:54:16 -0500

    adding saved.js file

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -7,5 +7,30 @@ class Saved extends React.Component {
         saved: [],
         btnColor: { background: '#4484CE' }
     }
+    componentDidMount() {
+        this.getSaved()
+    }
+    getSaved = () => {
+        API.getAllBooks()
+            .then(res => {
+                this.setState({ saved: res.date })
+            })
+    }
+    render() {
+        return (
+            <div>
+                <SavedHeader />
+                <Results
+                    books={this.state.saved}
+                    status="Saved Books:"
+                    buttonText="Delete"
+                    buttonColor={this.state.btnColor}
+                    getSaved={this.getSaved}
+                />
+            </div>
+        )
+    }
 
 }
+
+export default Saved
