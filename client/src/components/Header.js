a906df96290f1d1bff6ccde2b23a54c8f82658b3
Dannielle Taylor <dannielletaylor@outlook.com>
2020-05-09 19:08:45 -0500

    styling main page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -9,8 +9,11 @@ class Header extends React.Component {
     render() {
         return (
             <div className="header">
-                <h1>Google Books Search</h1>
-                <h2>Search for and Save Books of Interest</h2>
+                <h1>Great Reads Book Search</h1>
+                <h2>"Reading Gives Us Someplace to go when we have to stay where we are" --Mason Cooley</h2>
+                <br></br>
+                <br></br>
+                <h3>To Use the Greate Reads Book Search: <br></br>1. Enter the title of a book to return the details from Google Books.<br></br> 2. Click the view button to purchase a copy. <br></br>3. Click the save button to save to your personal list</h3>
                 <form>
                     <div className="input-group">
                         <input className="form-control" type="text" placeholder="Find a Book" value={this.props.search} onChange={this.props.updateInput}></input>
