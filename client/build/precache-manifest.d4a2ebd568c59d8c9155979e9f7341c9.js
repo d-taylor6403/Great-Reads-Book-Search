22653dc4652bc6eb4001f5be7bc68382680e659a
d-taylor6403 <dannielletaylor@outlook.com>
2020-07-08 18:13:44 -0500

    adding mlab access

---
+++ client/build/precache-manifest.d4a2ebd568c59d8c9155979e9f7341c9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3beb91f6add89e82b3c6",
+    "url": "/static/css/main.95063322.chunk.css"
+  },
+  {
+    "revision": "3beb91f6add89e82b3c6",
+    "url": "/static/js/main.0e7e1125.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b9787b27fb51c08c3e3a",
+    "url": "/static/js/2.73279cee.chunk.js"
+  },
+  {
+    "revision": "b633c8b109b7dd44c050de2a36467f81",
+    "url": "/static/media/headerImg.b633c8b1.jpg"
+  },
+  {
+    "revision": "98731bff6216c82b08758b9a4f909f16",
+    "url": "/static/media/savedbooks.98731bff.jpg"
+  },
+  {
+    "revision": "3eda78c0b3aee352f1119f748159a58b",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const axios = require("axios")
 //require models
 const db = require("./models");
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 // Serve up static assets (usually on heroku)
@@ -16,9 +16,9 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 //Connect to MongoDB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/book";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://user1:cakepop143@ds153719.mlab.com:53719/heroku_jrs6qzm0";
 mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true }, (err) => {
+  .connect(MONGODB_URI, {useNewUrlParser: true}, (err) => {
     if (err) throw err;
     console.log("database connected")
   })
@@ -28,7 +28,7 @@ mongoose
 //search route
 app.get("/search/:search", (req, res) => {
   let search = req.params.search;
-  axios.get("https://www.googleapis.com/books/v1/volumes?q=" + search).then(function (response) {
+  axios.get("https://www.googleapis.com/books/v1/volumes?q=" + search).then(function(response) {
     let books = response.data.items
     // console.log(books)
     let array = [];
@@ -53,14 +53,14 @@ app.get("/search/:search", (req, res) => {
 //get all saved books from db
 app.get("/api/books", (req, res) => {
   db.Book
-    .find({ saved: true })
+    .find({saved: true})
     .then(dbBook => res.json(dbBook))
     .catch(err => res.json(err))
 })
 //save a book to db
 app.post("/api/books/:id", (req, res) => {
   db.Book
-    .findOneAndUpdate({ _id: req.params.id }, { saved: true }, { new: true })
+    .findOneAndUpdate({_id: req.params.id}, {saved: true}, {new: true})
     .then(dbBook => {
       res.json(dbBook)
     })
@@ -68,13 +68,13 @@ app.post("/api/books/:id", (req, res) => {
 //delete all unsaved books from db
 app.delete("/api/books", (req, res) => {
   db.Book
-    .deleteMany({ saved: false })
+    .deleteMany({saved: false})
     .then(dbBooks => res.json(dbBooks))
 })
 //delete a book from db /api/books/:id
 app.delete("/api/books/:id", (req, res) => {
   db.Book
-    .deleteOne({ _id: req.params.id })
+    .deleteOne({_id: req.params.id})
     .then(dbBook => res.json(dbBook))
 })
 
