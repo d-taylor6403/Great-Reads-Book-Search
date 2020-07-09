c34211bcc2262057ae427875c5523763116673a6
d-taylor6403 <dannielletaylor@outlook.com>
2020-07-08 19:23:59 -0500

    fixing deployment error

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,23 +2,23 @@ import axios from "axios";
 
 export default {
     //get books from the database
-    getAllBooks: function () {
+    getAllBooks: function() {
         return axios.get("/api/books")
     },
     //save a book to the database
-    saveBook: function (id) {
+    saveBook: function(id) {
         return axios.post(`/api/books/${id}`)
     },
     //search google books
-    searchBooks: function (search) {
+    searchBooks: function(search) {
         return axios.get(`/search/${search}`)
     },
     //delete all unsaved book
-    deleteAllUnsaved: function () {
+    deleteAllUnsaved: function() {
         return axios.delete("/api/books")
     },
     //delete one book
-    deleteBook: function (id) {
+    deleteBook: function(id) {
         return axios.delete(`/api/books/${id}`)
     }
 }
